112c9f3dc88518cc92170a2cc0c40d59a8f34eb0
Michael <1185694600@qq.com>
2023-03-10 00:12:47 +0800

    update: build http client

--- springd/src/error.rs
+++ springd/src/error.rs
@@ -1,5 +1,3 @@
-use std::error::Error as StdError;
-
-pub type Error = Box<dyn StdError + Send + Sync + 'static>;
+pub type Error = Box<dyn std::error::Error + Send + Sync + 'static>;
 
 pub type Result<T> = std::result::Result<T, Error>;
--- springd/src/task.rs
+++ springd/src/task.rs
@@ -4,6 +4,7 @@ use reqwest::{
     header::{HeaderMap, HeaderValue},
     Client,
 };
+use std::fs;
 
 pub mod dispatcher;
 pub mod limiter;
@@ -15,34 +16,54 @@ pub struct Task<'a> {
 }
 
 impl<'a> Task<'a> {
-    pub fn new(arg: &'a Arg) -> Self {
-        Self {
+    pub fn new(arg: &'a Arg) -> crate::error::Result<Self> {
+        Ok(Self {
             arg,
+            client: Self::build_client(arg)?,
             dispatcher: Box::new(CountDispatcher::new()),
-        }
+        })
     }
 
     fn build_client(arg: &'a Arg) -> crate::error::Result<Client> {
-        let builder = Client::builder();
+        let mut builder = Client::builder();
 
         // build headers
         let mut headers = HeaderMap::new();
         for header in &arg.headers {
-            let mut parts: Vec<_> = header.trim().split(':').collect();
-            if parts.len() == 0 {
-                continue;
-            }
-
-            let value: HeaderValue;
-            if parts.len() == 1 {
-                value = HeaderValue::from_static("");
-            } else {
+            let parts: Vec<_> = header.trim().split(':').collect();
+            let mut value: HeaderValue = HeaderValue::from_static("");
+            if parts.len() == 2 {
                 value = HeaderValue::from_str(parts[1])?;
             }
             headers.insert(parts[0], value);
         }
 
-        Ok(Client {})
+        // disable http keep alive
+        if arg.disable_keep_alive {
+            headers.insert("Connection", HeaderValue::from_static("Close"));
+        }
+
+        builder = builder
+            .default_headers(headers)
+            .timeout(arg.timeout)
+            .connect_timeout(arg.timeout)
+            .danger_accept_invalid_certs(arg.insecure)
+            .danger_accept_invalid_hostnames(arg.insecure);
+
+        // use client certificates
+        if let Some(cert) = &arg.cert {
+            if let Some(key) = &arg.key {
+                let cert = fs::read(cert)?;
+                let key = fs::read(key)?;
+                let pkcs8 = reqwest::Identity::from_pkcs8_pem(&cert, &key)?;
+                builder = builder.identity(pkcs8);
+            }
+        }
+
+        match builder.build() {
+            Ok(client) => Ok(client),
+            Err(e) => Err(Box::new(e)),
+        }
     }
 
     async fn worker(&self) {}
