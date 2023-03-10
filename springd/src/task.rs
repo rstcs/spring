3d01c25e2155c924c323ddc4f5679b9460ad3da3
gamelife1314 <1185694600@qq.com>
2023-03-10 15:00:52 +0800

    update build client

--- springd/src/task.rs
+++ springd/src/task.rs
@@ -1,70 +1,81 @@
 use crate::task::dispatcher::{CountDispatcher, Dispatcher};
 use crate::Arg;
 use reqwest::{
-    header::{HeaderMap, HeaderValue},
-    Client,
+    header::{HeaderMap, HeaderName, HeaderValue},
+    Client, Request,
 };
 use std::fs;
 
 pub mod dispatcher;
 pub mod limiter;
 
-pub struct Task<'a> {
-    arg: &'a Arg,
+pub struct Task {
+    arg: Arg,
     client: Client,
     dispatcher: Box<dyn Dispatcher>,
 }
 
-impl<'a> Task<'a> {
-    pub fn new(arg: &'a Arg) -> crate::error::Result<Self> {
+impl Task {
+    pub fn new(arg: Arg, client: Client) -> anyhow::Result<Self> {
         Ok(Self {
             arg,
-            client: Self::build_client(arg)?,
+            client,
             dispatcher: Box::new(CountDispatcher::new()),
         })
     }
 
-    fn build_client(arg: &'a Arg) -> crate::error::Result<Client> {
-        let mut builder = Client::builder();
+    async fn worker(&self) {}
+}
 
-        // build headers
-        let mut headers = HeaderMap::new();
-        for header in &arg.headers {
-            let parts: Vec<_> = header.trim().split(':').collect();
-            let mut value: HeaderValue = HeaderValue::from_static("");
-            if parts.len() == 2 {
-                value = HeaderValue::from_str(parts[1])?;
-            }
-            headers.insert(parts[0], value);
+fn build_client(arg: &Arg) -> anyhow::Result<Client> {
+    let mut builder = Client::builder();
+
+    // build headers
+    let mut headers = HeaderMap::new();
+    for header in &arg.headers {
+        let parts: Vec<_> = header.trim().split(':').collect();
+        let mut value: HeaderValue = HeaderValue::from_static("");
+        if parts.len() == 2 {
+            value = HeaderValue::from_str(parts[1])?;
         }
+        headers.insert(HeaderName::from_bytes(parts[0].as_bytes())?, value);
+    }
 
-        // disable http keep alive
-        if arg.disable_keep_alive {
-            headers.insert("Connection", HeaderValue::from_static("Close"));
-        }
+    // disable http keep alive
+    if arg.disable_keep_alive {
+        headers.insert("Connection", HeaderValue::from_static("Close"));
+    }
 
-        builder = builder
-            .default_headers(headers)
-            .timeout(arg.timeout)
-            .connect_timeout(arg.timeout)
-            .danger_accept_invalid_certs(arg.insecure)
-            .danger_accept_invalid_hostnames(arg.insecure);
+    builder = builder
+        .default_headers(headers)
+        .timeout(arg.timeout)
+        .connect_timeout(arg.timeout)
+        .danger_accept_invalid_certs(arg.insecure)
+        .danger_accept_invalid_hostnames(arg.insecure);
 
-        // use client certificates
-        if let Some(cert) = &arg.cert {
-            if let Some(key) = &arg.key {
-                let cert = fs::read(cert)?;
-                let key = fs::read(key)?;
-                let pkcs8 = reqwest::Identity::from_pkcs8_pem(&cert, &key)?;
-                builder = builder.identity(pkcs8);
-            }
-        }
-
-        match builder.build() {
-            Ok(client) => Ok(client),
-            Err(e) => Err(Box::new(e)),
+    // use client certificates
+    if let Some(cert) = &arg.cert {
+        if let Some(key) = &arg.key {
+            let cert = fs::read(cert)?;
+            let key = fs::read(key)?;
+            let pkcs8 = reqwest::Identity::from_pkcs8_pem(&cert, &key)?;
+            builder = builder.identity(pkcs8);
         }
     }
 
-    async fn worker(&self) {}
+    match builder.build() {
+        Ok(client) => Ok(client),
+        Err(e) => Err(Box::new(e).into()),
+    }
+}
+
+fn build_request(arg: &Arg, client: &Client) -> anyhow::Result<Request> {
+    let mut builder = client.request(
+        arg.method.to_reqwest_method(),
+        arg.url.as_ref().unwrap().clone(),
+    );
+    match builder.build() {
+        Ok(client) => Ok(client),
+        Err(e) => Err(Box::new(e).into()),
+    }
 }
