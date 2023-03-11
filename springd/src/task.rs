76ff647950ea0c399c13f4fa66e71ceba1a67b0d
gamelife1314 <1185694600@qq.com>
2023-03-11 15:57:04 +0800

    stash task run implement

--- springd/src/task.rs
+++ springd/src/task.rs
@@ -1,10 +1,14 @@
 use crate::task::dispatcher::{CountDispatcher, Dispatcher};
 use crate::Arg;
+use bytes::Bytes;
+use log::{debug, error};
 use reqwest::{
     header::{HeaderMap, HeaderName, HeaderValue},
-    Client, Request,
+    multipart, Body, Client, Request, RequestBuilder,
 };
-use std::fs;
+use std::collections::HashMap;
+use tokio::{self, fs as tfs};
+use tokio_util::codec::{BytesCodec, FramedRead};
 
 pub mod dispatcher;
 pub mod limiter;
@@ -16,27 +20,64 @@ pub struct Task {
 }
 
 impl Task {
-    pub fn new(arg: Arg, client: Client) -> anyhow::Result<Self> {
+    pub async fn new(arg: Arg) -> anyhow::Result<Self> {
+        let client = build_client(&arg).await?;
+        let dispatcher =
+            Box::new(CountDispatcher::new(arg.requests.unwrap(), &arg.rate));
         Ok(Self {
             arg,
             client,
-            dispatcher: Box::new(CountDispatcher::new()),
+            dispatcher,
         })
+    }
+
+    async fn worker(&self) {
+        loop {
+            if !self.dispatcher.try_apply_job().await {
+                break;
+            }
+
+            let request = build_request(&self.arg, &self.client).await;
+            if request.is_err() {
+                panic!("unknown fatal error: {:?}", request.err());
+            }
+
+            let response = self.client.execute(request.unwrap()).await;
+            if response.is_err() {
+                error!("execute request failed: {:?}", response.err());
+            } else {
+                let response = response.unwrap();
+                debug!(
+                    "execute request succeeded, status: {:?}, headers: {:?}",
+                    response.status(),
+                    response.headers()
+                );
+            }
+        }
+    }
+
+    pub fn run(&self) -> anyhow::Result<()> {
+        let mut tasks = Vec::with_capacity(self.arg.connections as usize);
+        for _ in 0..self.arg.connections {
+            tasks.push(tokio::spawn((&self).worker()));
+        }
+        Ok(())
     }
 }
 
-fn build_client(arg: &Arg) -> anyhow::Result<Client> {
+async fn build_client(arg: &Arg) -> anyhow::Result<Client> {
     let mut builder = Client::builder();
 
     // build headers
     let mut headers = HeaderMap::new();
     for header in &arg.headers {
-        let parts: Vec<_> = header.trim().split(':').collect();
-        let mut value: HeaderValue = HeaderValue::from_static("");
-        if parts.len() == 2 {
-            value = HeaderValue::from_str(parts[1])?;
+        let parts = header.trim().split_once(':');
+        if let Some(parts) = parts {
+            headers.insert(
+                HeaderName::from_bytes(parts.0.as_bytes())?,
+                HeaderValue::from_str(parts.1)?,
+            );
         }
-        headers.insert(HeaderName::from_bytes(parts[0].as_bytes())?, value);
     }
 
     // disable http keep alive
@@ -54,8 +95,8 @@ fn build_client(arg: &Arg) -> anyhow::Result<Client> {
     // use client certificates
     if let Some(cert) = &arg.cert {
         if let Some(key) = &arg.key {
-            let cert = fs::read(cert)?;
-            let key = fs::read(key)?;
+            let cert = tfs::read(cert).await?;
+            let key = tfs::read(key).await?;
             let pkcs8 = reqwest::Identity::from_pkcs8_pem(&cert, &key)?;
             builder = builder.identity(pkcs8);
         }
@@ -67,11 +108,121 @@ fn build_client(arg: &Arg) -> anyhow::Result<Client> {
     }
 }
 
-fn build_request(arg: &Arg, client: &Client) -> anyhow::Result<Request> {
+async fn set_request_text_body(
+    arg: &Arg,
+    mut builder: RequestBuilder,
+) -> anyhow::Result<RequestBuilder> {
+    if let Some(text_body) = &arg.text_body {
+        builder = builder
+            .body(Bytes::from(text_body.clone()))
+            .header("Content-Type", "text/plain; charset=UTF-8");
+    }
+
+    if let Some(text_file) = &arg.text_file {
+        let file = tfs::File::open(text_file).await?;
+        builder = builder
+            .body(file)
+            .header("Content-Type", "text/plain; charset=UTF-8");
+    }
+
+    Ok(builder)
+}
+
+async fn set_request_json_body(
+    arg: &Arg,
+    mut builder: RequestBuilder,
+) -> anyhow::Result<RequestBuilder> {
+    if let Some(json_body) = &arg.json_body {
+        builder = builder
+            .body(Bytes::from(json_body.clone()))
+            .header("Content-Type", "application/json; charset=UTF-8");
+    }
+
+    if let Some(json_file) = &arg.json_file {
+        let file = tfs::File::open(json_file).await?;
+        builder = builder
+            .body(file)
+            .header("Content-Type", "application/json; charset=UTF-8");
+    }
+
+    Ok(builder)
+}
+
+async fn set_request_form_body(
+    arg: &Arg,
+    mut builder: RequestBuilder,
+) -> anyhow::Result<RequestBuilder> {
+    if !arg.form.is_empty() {
+        let mut params = HashMap::new();
+        for kv in &arg.form {
+            let parts = kv.trim().split_once(':');
+            if let Some(v) = parts {
+                params.insert(v.0, v.1);
+            }
+        }
+        builder = builder.form(&params);
+    }
+
+    Ok(builder)
+}
+
+async fn set_request_multipart_body(
+    arg: &Arg,
+    mut builder: RequestBuilder,
+) -> anyhow::Result<RequestBuilder> {
+    if !arg.mp.is_empty() || !arg.mp_file.is_empty() {
+        let mut form = multipart::Form::new();
+        for kv in &arg.mp {
+            let parts = kv.trim().split_once(':');
+            if let Some(parts) = parts {
+                let k = parts.0.to_string().clone().to_owned();
+                let v = parts.0.to_string().clone().to_owned();
+                form = form.text(k, v);
+            }
+        }
+
+        // for uploading file
+        for f in &arg.mp_file {
+            let f = f.clone().to_owned();
+            let name = f.file_name().unwrap().to_str().unwrap().to_owned();
+            let name_copy = name.clone();
+
+            let file = tfs::File::open(&f).await?;
+            let stream = FramedRead::new(file, BytesCodec::new());
+            let file_body = Body::wrap_stream(stream);
+
+            // get file mime information
+            let mime = mime_guess::from_path(&f);
+            let mime = mime.first_or_octet_stream();
+            let mime = mime.type_();
+            let mime = mime.as_ref();
+
+            form = form.part(
+                name,
+                multipart::Part::stream(file_body)
+                    .file_name(name_copy)
+                    .mime_str(mime)?,
+            );
+        }
+        builder = builder.multipart(form);
+    }
+
+    Ok(builder)
+}
+
+async fn build_request(arg: &Arg, client: &Client) -> anyhow::Result<Request> {
     let mut builder = client.request(
         arg.method.to_reqwest_method(),
         arg.url.as_ref().unwrap().clone(),
     );
+
+    // the following four types are mutually exclusive
+    // only one will take effect
+    builder = set_request_text_body(arg, builder).await?;
+    builder = set_request_form_body(arg, builder).await?;
+    builder = set_request_json_body(arg, builder).await?;
+    builder = set_request_multipart_body(arg, builder).await?;
+
     match builder.build() {
         Ok(client) => Ok(client),
         Err(e) => Err(Box::new(e).into()),
--- springd/src/task/dispatcher.rs
+++ springd/src/task/dispatcher.rs
@@ -1,43 +1,120 @@
 //! dispatcher module is used to distribute tasks according to different models
 
-pub trait Dispatcher {
+use async_trait::async_trait;
+use governor::{
+    clock::DefaultClock,
+    state::{direct::NotKeyed, InMemoryState},
+    Quota, RateLimiter,
+};
+use std::num::NonZeroU32;
+use std::sync::atomic::{AtomicBool, AtomicU64, Ordering::*};
+use tokio::time;
+
+type Limiter = RateLimiter<NotKeyed, InMemoryState, DefaultClock>;
+
+#[async_trait]
+pub trait Dispatcher: Send + Sync {
     /// query current task process, returning 0 to 1
     fn get_process(&self) -> f64;
 
     /// worker apply a job from dispatcher, return true continue to handle,
     /// return false worker will exit.
-    fn try_apply_job(&mut self) -> bool;
+    async fn try_apply_job(&self) -> bool;
 
     /// when worker complete job, it will notify the dispatcher
-    fn notify_complete_job(&mut self);
+    fn complete_job(&mut self);
 
     /// when the program receives an external termination signal, notify the
     /// Dispatcher to process it
     fn cancel(&mut self);
 }
 
-pub struct CountDispatcher {}
+pub struct CountDispatcher {
+    /// total requests number will send to server
+    total: u64,
+
+    /// number of jobs already applied for
+    applied: AtomicU64,
+
+    /// the amount of work done
+    completed: AtomicU64,
+
+    /// indicates whether it is canceled
+    is_canceled: AtomicBool,
+
+    /// indicate whether to complete
+    is_done: AtomicBool,
+
+    /// a rate limiter that limits the acquisition of a fixed number of tokens
+    /// per second
+    limiter: Option<Limiter>,
+}
 
 impl CountDispatcher {
-    pub fn new() -> Self {
-        Self {}
+    pub fn new(total: u64, rate: &Option<u16>) -> Self {
+        let mut limiter: Option<Limiter> = None;
+        if let Some(rate) = rate {
+            limiter = Some(RateLimiter::direct(Quota::per_second(
+                NonZeroU32::new(*rate as u32).unwrap(),
+            )));
+        }
+        Self {
+            limiter,
+            total,
+            applied: AtomicU64::new(0),
+            completed: AtomicU64::new(0),
+            is_canceled: AtomicBool::new(false),
+            is_done: AtomicBool::new(false),
+        }
     }
 }
 
+#[async_trait]
 impl Dispatcher for CountDispatcher {
     fn get_process(&self) -> f64 {
-        todo!()
+        self.completed.load(Acquire) as f64 / self.total as f64
     }
 
-    fn try_apply_job(&mut self) -> bool {
-        todo!()
+    async fn try_apply_job(&self) -> bool {
+        if self.is_done.load(Acquire) || self.is_canceled.load(Acquire) {
+            return false;
+        }
+
+        // is there any chance of apply a job
+        if self.applied.load(Acquire) < self.total {
+            let previous = self.applied.fetch_add(1, SeqCst);
+            if previous >= self.total {
+                return false;
+            }
+        }
+
+        // if set the maximum rate, need to check whether it is currently
+        // possible to send the request
+        if let Some(limiter) = &self.limiter {
+            loop {
+                let result = limiter.check();
+                if result.is_ok() {
+                    break;
+                }
+                time::sleep(time::Duration::from_nanos(100)).await;
+            }
+        }
+
+        true
     }
 
-    fn notify_complete_job(&mut self) {
-        todo!()
+    fn complete_job(&mut self) {
+        let _ = self.completed.fetch_add(1, SeqCst);
+        if self.completed.load(SeqCst) >= self.total
+            && !self.is_done.load(SeqCst)
+        {
+            self.is_done.store(true, SeqCst);
+        }
     }
 
     fn cancel(&mut self) {
-        todo!()
+        if !self.is_canceled.load(Acquire) {
+            self.is_canceled.store(true, SeqCst);
+        }
     }
 }
